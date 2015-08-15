91174ee9c4c42a185f3fd156d43fceee16c0e598
Myztiq <github@ryan-kahn.com>
2015-08-15 14:20:53 -0700

    Updated to load data from prismic.

--- app.js
+++ app.js
@@ -2,21 +2,19 @@
 /**
  * Module dependencies.
  */
-var express = require('express'),
-    favicon = require('static-favicon'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    session = require('express-session'),
-    errorHandler = require('errorhandler'),
-    routes = require('./routes'),
-    http = require('http'),
-    path = require('path'),
-    prismic = require('./prismic-helpers');
+var express = require('express');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var errorHandler = require('errorhandler');
+var routes = require('./lib/routes');
+var path = require('path');
+var sassMiddleware = require('node-sass-middleware');
 
 var app = express();
-
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -27,15 +25,17 @@ app.use(bodyParser());
 app.use(methodOverride());
 app.use(cookieParser('1234'));
 app.use(session({secret: '1234', saveUninitialized: true, resave: true}));
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'styles'),
+  dest: path.join(__dirname, 'public'),
+  debug: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(errorHandler());
 
-// Routes
-app.route('/').get(routes.index);
-app.route('/documents/:id/:slug').get(routes.detail);
-app.route('/search').get(routes.search);
-app.route('/preview').get(routes.preview);
+routes.bind(app);
 
 var PORT = app.get('port');
 
